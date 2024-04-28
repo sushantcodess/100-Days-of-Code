4dcb9aa1a6c691a9a10a8cd260b8f39c2431241d
Sushant Aggarwal <sushant.aggarwal2021@vitbhopal.ac.in>
2024-04-28 23:56:35 +0530

    DAY-8 Prefinal

--- DAY 8/TASK-1.py
+++ DAY 8/TASK-1.py
@@ -1,13 +1,89 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+print("Welcome to HangMan!")
+word_list = ["dani", "sybil", "jia", "stacy", "ariana", "valentina", "scarlit"]
 chosen_word = random.choice(word_list)
 
-guess = input("Guess a letter: ").lower()
-
-
+#print(chosen_word)
+blank = []
 for letter in chosen_word:
-    if guess == letter:
-        print("right")
-    else:
-        print("wrong")
+    blank += "_"
+print(blank)
+
+lives = 6
+
+while "_" in blank:
+    guess = input("Guess a letter: ").lower()
+    indi = -1
+    for letter in chosen_word:
+        indi += 1
+        if guess == letter:
+            blank[indi] = letter        
+    print(blank)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(stages[lives+1])
+        print(f"You lost a life! {lives} lifes remaining...")
+        if lives == 0:
+            print("You loose!")
+            break
+    if "_" not in blank:
+        print("Yayy you have won!")
