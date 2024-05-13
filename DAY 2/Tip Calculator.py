7b3a2463531f21d938e13fb5e960d6c61997364a
Sushant Aggarwal <sushant.aggarwal2021@vitbhopal.ac.in>
2024-05-13 22:50:56 +0530

    delted extra file

--- DAY 2/Tip Calculator.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to the Tip Calculator!")
-amount = float(input("What was the total bill amount? ₹"))
-tip = int(input("How much tip would you like to give? 0, 10, 12, 15? "))
-people = int(input("How many people to split the bill? "))
-
-amount_with_tip = amount + tip/100 * amount
-bill = amount_with_tip / people
-
-print(f"Each person should pay {bill} \n₹")
