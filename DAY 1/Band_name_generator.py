7586a163af40e68ac7637f12e60c85426842d2fd
Sushant Aggarwal <sushant.aggarwal2021@vitbhopal.ac.in>
2024-05-13 22:39:45 +0530

    DAY 1

---
+++ DAY 1/Band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name generator!!!")
+City = input("Enter the name of the city you grew up in! \n")
+pet = input("Enter the name of your pet! \n")
+print("The name of your band can be " + City + " " + pet)
